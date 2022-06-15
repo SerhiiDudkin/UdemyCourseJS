0c6bdd3cf87fa1ee001b3c7343cd882febce0ceb
Serhii Dudkin <serzh1.12.73.@gmail.com>
2022-06-15 21:32:38 +0300

    add two task

--- homework2Function.js
+++ homework2Function.js
@@ -40,3 +40,41 @@ function getMathResult(num, times) {
 
     return str;
 }
+
+function calculateVolumeAndArea(side) {
+    if (typeof side === "number" && side > 0 && Number.isInteger(side)) {
+        const volume = side * side * side;
+        const area = side * side * 6;
+        return `Объем куба: ${volume}, площадь всей поверхности: ${area}`
+    } else {
+        return `При вычислении произошла ошибка`
+    }
+}
+//console.log(calculateVolumeAndArea());
+
+function getCoupeNumber(numberPlace) {
+    if (typeof numberPlace !== "number" || !Number.isInteger(numberPlace) || numberPlace < 0) {
+       return  console.log('Ошибка. Проверьте правильность введенного номера места');
+    } else if (numberPlace === 0 || numberPlace > 36) {
+       return  console.log('Таких мест в вагоне не существует');
+    } else if (numberPlace >= 1 && numberPlace <= 4) {
+        return console.log(1);
+    } else if (numberPlace >= 5 && numberPlace <= 8) {
+        return console.log(2);
+    } else if (numberPlace >=9 && numberPlace <= 12) {
+        return console.log(3);
+    } else if (numberPlace >=13 && numberPlace <= 16) {
+        return console.log(4);
+    } else if (numberPlace >= 17 && numberPlace <= 20) {
+        return console.log(5);
+    } else if (numberPlace >= 21 && numberPlace <= 24) {
+        return console.log(6);
+    } else if (numberPlace >=25 && numberPlace <= 28) {
+        return console.log(7);
+    } else if (numberPlace >= 29 && numberPlace <= 32) {
+        return console.log(8);
+    } else if (numberPlace >= 33 && numberPlace <= 36) {
+        return console.log(9);
+    }
+}
+//getCoupeNumber();
